ed0869e7af0544c2137f8c60697d6af85456c7cd
Marcinho <marciohamer@gmail.com>
2022-08-21 21:41:15 -0300

    Ajustado logica do round da hora no tempo por mes

--- src/timer.py
+++ src/timer.py
@@ -5,7 +5,6 @@ from RecordClasse import Record
 from MainStramVideo import MAIN_VIDEO_STREAM
 
 
-#Rever essa lógica
 def verify():
     for vaga in MAIN_VIDEO_STREAM.lista_de_vagas:
         if vaga.emUso == True:
@@ -14,9 +13,8 @@ def verify():
                 vaga.record.append(new_record)
             for record in vaga.record:
                 if record.data == datetime.now().strftime("%d/%m/%Y"):
-                    record.tempoUsado +=1
-                    vaga.tempoPorMes[int(record.mes) - 1] += 0.00027
-                    vaga.tempoPorMes[int(record.mes) - 1] = round(vaga.tempoPorMes[int(record.mes) - 1])
+                    record.tempoUsado +=1 # por segundo
+                    vaga.tempoPorMes[int(record.mes) - 1] += 0.00027 #por hora
             data_hoje = [rec for rec in vaga.record if rec.data == datetime.now().strftime("%d/%m/%Y")]
             if data_hoje == []:
                 record = Record(tempoUsado=0, vaga=vaga.id)
